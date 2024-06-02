0219d3fe70098eca03afaf81b5cf405b225e8700
rootnure <nur.diu.2791@gmail.com>
2024-06-02 17:34:37 +0600

    Intersection type in TS

--- module1/src/1.10.ts
+++ module1/src/1.10.ts
@@ -44,6 +44,29 @@
 
 
 
+  // Intersection Type
+
+  type FrontendDeveloper = {
+    skills: string[];
+    designation1: 'Frontend Developer';
+  }
+
+  type BackendDeveloper = {
+    skills: string[];
+    designation2: 'Backend Developer';
+  }
+
+  type FullstackDeveloper = FrontendDeveloper & BackendDeveloper;
+
+  const developer1: FullstackDeveloper = {
+    skills: ['HTML', 'CSS', 'Express', 'Mongoose'],
+    designation1: 'Frontend Developer',
+    designation2: 'Backend Developer',
+  }
+
+  console.log({ developer1 });
+
+
 
 
 
