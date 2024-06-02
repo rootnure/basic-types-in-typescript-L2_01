6089aa0af90557ae7996026643b92f5215d03ad4
rootnure <nur.diu.2791@gmail.com>
2024-06-02 16:51:30 +0600

    Type Alias for object

--- module1/src/1.9.ts
+++ module1/src/1.9.ts
@@ -7,11 +7,67 @@
     bangali: 88,
     english: 71
   };
-
   const { mathematics: math } = myScores; // Change name while performing destructuring
   console.log(math);
 
 
+
+
+  // Type Alias
+
+  /*
+  const student1: {
+    name: string;
+    age: number;
+    gender: string;
+    contactNo: string;
+    address: string;
+  } = {
+    name: 'Nur',
+    age: 25,
+    gender: 'male',
+    contactNo: '01800000000',
+    address: 'Dhaka, BD',
+  };
+  
+  const student2: {
+    name: string;
+    age: number;
+    gender: string;
+    address: string;
+  } = {
+    name: 'Rifat',
+    age: 26,
+    gender: 'male',
+    address: 'Dinajpur, BD',
+  }
+  */
+
+
+  // Instead of declaring type multiple times, declare a type and use that type multiple times
+
+  type Student = {
+    name: string;
+    age: number;
+    gender: string;
+    contactNo?: string;
+    address: string;
+  }
+
+  const student1: Student = {
+    name: 'Nur',
+    age: 25,
+    gender: 'male',
+    contactNo: '01800000000',
+    address: 'Dhaka, BD',
+  };
+
+  const student2: Student = {
+    name: 'Rifat',
+    age: 26,
+    gender: 'male',
+    address: 'Dinajpur, BD',
+  }
 
 
 
