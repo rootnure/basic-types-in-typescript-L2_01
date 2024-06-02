107771ff0b23b731a51a2cee1a768f9315756349
rootnure <nur.diu.2791@gmail.com>
2024-06-02 17:55:59 +0600

    optional chaining, optional chaining with nullish coalescing

--- module1/src/1.11.ts
+++ module1/src/1.11.ts
@@ -9,12 +9,13 @@
     // console.log({isAdult: false});
   }
 
-  // ternary operator
+  /* ternary operator */
   const isAdult = age >= 18 ? true : false;
   // console.log({ isAdult: isAdult });
 
-  // nullish coalescing operator
+  /* nullish coalescing operator */
   // use only when need to make a decision based on null or undefined
+
   // const isAuthenticated = null;
   // const isAuthenticated = undefined;
   // const isAuthenticated = '';
@@ -22,9 +23,33 @@
 
   const result1 = isAuthenticated ?? 'Guest'; // only work --> null / undefined
   const result2 = isAuthenticated ? isAuthenticated : 'Guest';
-  console.log({ result1, result2 });
+  // console.log({ result1, result2 });
 
+  /* optional chaining */
 
+  type User = {
+    name: string;
+    address: {
+      city: string;
+      road: string;
+      presentAddress: string;
+      permanentAddress?: string;
+    }
+  }
+
+  const user: User = {
+    name: 'Fahim',
+    address: {
+      city: 'DHK',
+      road: '',
+      presentAddress: 'DHK'
+    }
+  }
+
+  // const permanentAddress = user?.address?.permanentAddress;
+  const permanentAddress = user?.address?.permanentAddress ?? 'No Permanent Address Found!!!';
+
+  console.log(permanentAddress);
 
 
   //
