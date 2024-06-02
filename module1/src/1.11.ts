80576e68481ec6b5ef9a72f91ad9918f6124f166
rootnure <nur.diu.2791@gmail.com>
2024-06-02 17:48:10 +0600

    ternary operator

---
+++ module1/src/1.11.ts
@@ -0,0 +1,20 @@
+{
+  // ternary operator || optional chaining || nullish coalescing operator
+
+  const age: number = 15;
+
+  if (age >= 18) {
+    console.log({ isAdult: true });
+  } else {
+    console.log({ isAdult: false });
+  }
+
+  // ternary operator
+  const isAdult = age >= 18 ? true : false;
+  console.log({ isAdult: isAdult });
+
+
+
+
+  //
+}
